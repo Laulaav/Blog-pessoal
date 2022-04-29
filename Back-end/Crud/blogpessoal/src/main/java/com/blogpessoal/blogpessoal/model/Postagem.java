990f45d733a9288f46d9f2b2ca5e5da3a508b413
Laura Araújo <laulaav@gmail.com>
2022-04-29 13:13:20 -0300

    Crud parte 1 e 2

---
+++ Back-end/Crud/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Postagem.java
@@ -0,0 +1,79 @@
+package com.blogpessoal.blogpessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message= "O título é obrigatório!")
+	@Size(min = 5, max = 100, message= "O título deve conter no mínimo 5 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message= "O texto é obrigatório!")
+	@Size(min = 10, max = 1000, message= "O título deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	private String foto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	
+}
